dff02d685ef40883d06f9d80284d6ba9bd6357ae
Chaahat <arorac2@mymacewan.ca>
2023-06-27 14:51:37 -0600

    remove redundant code

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -11,10 +11,6 @@ import '../styles/TopNavigationBar.scss'
 
 const TopNavigation = ({ favourites, notificationCount}) => {
   const [favoriteCount, setFavoriteCount] = useState(favourites ? favourites.length : 0);
-  const showNotification = Array.isArray(favourites) && favourites.length > 0;
-  const handleFavorite = () => {
-    setFavoriteCount(prevCount => prevCount + 1);
-  };
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
